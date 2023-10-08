6566eb746b6403200f0efd89c833968794aad5d2
Jonghwa <dvlpsjh@gmail.com>
2023-10-08 17:11:32 +0900

    쿠폰 1인 1매 제한하기 - redis set

---
+++ api/src/main/java/com/mnok/api/repository/AppliedUserRepository.java
@@ -0,0 +1,18 @@
+package com.mnok.api.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class AppliedUserRepository {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public Long add(Long userId) {
+        return redisTemplate
+                .opsForSet()
+                .add("applied_user", userId.toString());
+    }
+}
+
